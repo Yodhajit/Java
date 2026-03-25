5e821730c020f667e578ed8004972431690fd1c0
Yodhajit <daslolman123@gmail.com>
2026-03-25 09:17:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = input.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+  }
