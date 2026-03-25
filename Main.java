40c59ca75a6635ab63548fdd4312297fb4c7c055
Yodhajit <daslolman123@gmail.com>
2026-03-25 09:44:42 +0530

    Add area calculation for a circle in Main.java

--- Main.java
+++ Main.java
@@ -7,3 +7,15 @@ public class Main {
         System.out.println("Hello " + name + "!");
     }
   }
+
+import java.util.Scanner;
+public class area_circle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Enter a radius of the circle: ");
+        int num = in.nextInt();
+        double area = 3.14 * (num*num);
+        System.out.println(area);
+        }
+}
+
