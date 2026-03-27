21a7bf3b7119ccac9905199a64a1277f1452e57b
Yodhajit <daslolman123@gmail.com>
2026-03-27 23:55:31 +0530

    Refactor Main.java to check for palindromes
    Removed circle area calculation and added palindrome check.

--- Main.java
+++ Main.java
@@ -1,21 +1,42 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter name: ");
-        String name = input.nextLine();
-        System.out.println("Hello " + name + "!");
-    }
-  }
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Enter name: ");
+//         String name = input.nextLine();
+//         System.out.println("Hello " + name + "!");
+//     }
+//   }
+
+// import java.util.Scanner;
+// public class area_circle {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         System.out.printf("Enter a radius of the circle: ");
+//         int num = in.nextInt();
+//         double area = 3.14 * (num*num);
+//         System.out.println(area);
+//         }
+// }
 
 import java.util.Scanner;
-public class area_circle {
+
+public class Palindrome {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.printf("Enter a radius of the circle: ");
+        
         int num = in.nextInt();
-        double area = 3.14 * (num*num);
-        System.out.println(area);
-        }
-}
+        int current = num, temp = 0;
 
+        while (num > 0) {
+            int digit = num % 10;
+            temp = temp * 10 + digit;
+            num /= 10;
+        }
+
+        if (current == temp)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
