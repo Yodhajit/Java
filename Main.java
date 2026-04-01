7ea14db89a473a1be34045d4ac3cdc75f144bdd5
Yodhajit <daslolman123@gmail.com>
2026-04-01 23:57:07 +0530

    Add ReverseNumber class to reverse an integer

--- Main.java
+++ Main.java
@@ -59,8 +59,31 @@ class Solution {
         while (n % 5 == 0) {
             n = n / 5;
         }
-
         return n == 1;
     }
 }
+
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int original = num;
+        int reversed = 0;
+        
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        
+        System.out.println("Original number: " + original);
+        System.out.println("Reversed number: " + reversed);
+    }
+}
+
+
+
 
