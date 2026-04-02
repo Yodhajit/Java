510bb8ffc8582111d41f29d6f0f05ec8383c853d
Yodhajit <daslolman123@gmail.com>
2026-04-02 23:56:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -41,49 +41,92 @@
 //     }
 // }
 // Leet code problem-
+// class Solution {
+//     public boolean isUgly(int n) {
+
+//         if (n <= 0) {
+//             return false;
+//         }
+
+//         while (n % 2 == 0) {
+//             n = n / 2;
+//         }
+
+//         while (n % 3 == 0) {
+//             n = n / 3;
+//         }
+
+//         while (n % 5 == 0) {
+//             n = n / 5;
+//         }
+//         return n == 1;
+//     }
+// }
+
+// import java.util.Scanner;
+// public class ReverseNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int num = sc.nextInt();
+        
+//         int original = num;
+//         int reversed = 0;
+        
+//         while (num != 0) {
+//             int digit = num % 10;
+//             reversed = reversed * 10 + digit;
+//             num /= 10;
+//         }
+        
+//         System.out.println("Original number: " + original);
+//         System.out.println("Reversed number: " + reversed);
+//     }
+// }
+//using functions
+
 class Solution {
-    public boolean isUgly(int n) {
+    public static boolean isArmstrong(int n) {
 
-        if (n <= 0) {
-            return false;
+        int original = n;
+        int count = 0;
+        int temp = n;
+
+        // Step 1: count digits
+        while (temp > 0) {
+            count++;
+            temp = temp / 10;
         }
 
-        while (n % 2 == 0) {
-            n = n / 2;
+        int sum = 0;
+        temp = n;
+
+        // Step 2: calculate sum of powers
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            int power = 1;
+            for (int i = 0; i < count; i++) {
+                power = power * digit;
+            }
+
+            sum = sum + power;
+            temp = temp / 10;
         }
 
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
-
-        while (n % 5 == 0) {
-            n = n / 5;
-        }
-        return n == 1;
+        return sum == original;
     }
-}
 
-import java.util.Scanner;
-public class ReverseNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        
-        int original = num;
-        int reversed = 0;
-        
-        while (num != 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num /= 10;
+        int n = 153;
+
+        if (isArmstrong(n)) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not Armstrong");
         }
-        
-        System.out.println("Original number: " + original);
-        System.out.println("Reversed number: " + reversed);
     }
 }
-
 
 
 
