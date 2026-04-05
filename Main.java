02e0a7b2cc9d95ec0cbbef465ed70efc0c5a5b88
Yodhajit <daslolman123@gmail.com>
2026-04-05 23:55:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -129,23 +129,49 @@
 // }
 
 //fibonacci series
+// class Main {
+//     public static void main(String[] args) {
+
+//         int n = 10;  
+
+//         int a = 0, b = 1;
+
+//         System.out.print(a + " " + b + " ");
+
+//         for (int i = 2; i < n; i++) {
+//             int c = a + b;
+//             System.out.print(c + " ");
+
+//             a = b;
+//             b = c;
+//         }
+//     }
+// }
+//triangle pattern
 class Main {
     public static void main(String[] args) {
 
-        int n = 10;  
+        int n = 5;
 
-        int a = 0, b = 1;
-
-        System.out.print(a + " " + b + " ");
-
-        for (int i = 2; i < n; i++) {
-            int c = a + b;
-            System.out.print(c + " ");
-
-            a = b;
-            b = c;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
+//inverted triangle pattern 
+class Main {
+    public static void main(String[] args) {
 
+        int n = 5;
 
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
