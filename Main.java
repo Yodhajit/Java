6b7483367f8d38aa9c4353708d0015ad8f728358
Yodhajit <daslolman123@gmail.com>
2026-04-16 23:38:04 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -245,3 +245,29 @@
 //     return 0;
 //     }
 // }
+
+//Leetcode Problem 
+// class Solution {
+//     public int addDigits(int num) {
+
+//         if (num < 10) {
+//             return num;
+//         }
+
+//         while (num >= 10) {
+
+//             int sum = 0;
+//             int temp = num;
+
+//             while (temp > 0) {
+//                 int digit = temp % 10;
+//                 sum += digit;
+//                 temp /= 10;
+//             }
+
+//             num = sum;
+//         }
+
+//         return num;
+//     }
+// }
